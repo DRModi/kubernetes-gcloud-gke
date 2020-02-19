7aea61d7003269a24ef7526db1c75cc9a4ace42c
Dixit Modi <dmodi.jmcs@gmail.com>
2020-02-19 08:43:32 -0500

    Microservice Spring cloud with Kubernetes

--- 05-currency-conversion-microservice-basic/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ 05-currency-conversion-microservice-basic/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -3,8 +3,9 @@ package com.in28minutes.microservices.currencyconversionservice.resource;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+//below will discovered by kuberentes at dynamically -by service discovery | DNS 
 @FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")//
+//the below one works, when services are created in order.
 //@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 //@FeignClient(name = "currency-exchange-service")//Kubernetes Service Name
 public interface CurrencyExchangeServiceProxy {
